9d332a06bf79626c534a8517f7c839c2ded73bfc
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-04-27 00:04:45 +0300

    Reorder import statements in ZTele.kt

--- src/main/kotlin/org/zoobastiks/ztelegram/ZTele.kt
+++ src/main/kotlin/org/zoobastiks/ztelegram/ZTele.kt
@@ -3,8 +3,8 @@ package org.zoobastiks.ztelegram
 import org.bukkit.Bukkit
 import org.zoobastiks.ztelegram.chat.ChatManager
 import org.zoobastiks.ztelegram.checkin.CheckinManager
-import org.bukkit.plugin.java.JavaPlugin
 import org.zoobastiks.ztelegram.commands.SendCommand
+import org.bukkit.plugin.java.JavaPlugin
 import org.zoobastiks.ztelegram.placeholders.TGBridgeExpansion
 import org.zoobastiks.ztelegram.bot.TBot
 import org.zoobastiks.ztelegram.cmd.TCmds
