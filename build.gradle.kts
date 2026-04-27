6fe429e02067ef6918cfff908b3b7418e102db2c
AllF1RE <111210671+AllFiRE0@users.noreply.github.com>
2026-04-27 16:38:11 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,29 +17,31 @@ version = "1.7"
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://repo.mrbrikster.dev/releases")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
     maven("https://jitpack.io")
     maven("https://repo.extendedclip.com/content/repositories/placeholderapi/")
+    maven("https://nexus.scarsz.me/content/groups/public/") {
+        content {
+            includeGroup("ru.brikster")
+        }
+    }
 }
 
 dependencies {
     implementation("org.telegram:telegrambots:6.9.7.1")
     implementation("org.json:json:20240303")
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT") // Совместимо с 1.21.11 (Paper API обратно совместим)
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
     implementation("net.kyori:adventure-api:4.17.0")
     implementation("net.kyori:adventure-text-minimessage:4.17.0")
     implementation("net.kyori:adventure-text-serializer-legacy:4.17.0")
-    // SQLite для универсальной базы данных
     implementation("org.xerial:sqlite-jdbc:3.44.1.0")
-    // bStats для статистики использования плагина
     implementation("org.bstats:bstats-bukkit:3.1.0")
     compileOnly("com.github.MilkBowl:VaultAPI:1.7") {
         exclude(group = "org.bukkit", module = "bukkit")
     }
     compileOnly("me.clip:placeholderapi:2.11.6")
-    compileOnly("ru.brikster.chatty:chatty-api:3.0.0")
+    compileOnly("ru.brikster:chatty-api:3.0.0-20240908.194144-1")
 }
 
 tasks {
@@ -47,9 +49,6 @@ tasks {
         archiveClassifier.set("")
         archiveBaseName.set("Ztelegram")
         mergeServiceFiles()
-        
-        // Релокация bstats не требуется - библиотека не конфликтует с другими плагинами
-        // Если нужна релокация, можно добавить: relocate("org.bstats", "${project.group}.bstats")
     }
 
     build {
